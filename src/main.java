d1340d9c640302b4b3202e429e318cb70d220d6f
Ramastix <ramastix@gmail.com>
2023-01-16 12:27:29 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        int prise = 4400;
+        int miles = 20;
+
+        int bonus = prise / miles;
+
+        System.out.println(bonus);
+
+    }
+}
